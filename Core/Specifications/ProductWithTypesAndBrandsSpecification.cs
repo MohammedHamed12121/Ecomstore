8b33e599a460559f22cd36759e724e069f5454cc
MohamedHamed <MohamedHamed205>
2023-11-20 17:58:08 +0200

    automapper1

---
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,5 +11,13 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
